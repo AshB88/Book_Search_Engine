d7fcf255fc903c51861b7233b5f15fe2b8f51d9a
Ash <ashleighb.jjd@gmail.com>
2025-03-24 18:03:03 -0400

    Fixed book saving issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ import Navbar from './components/Navbar';
 
 // Construct main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'http://localhost:3001/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
@@ -31,6 +31,7 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
+  uri: 'http://localhost:3001/graphql',
   cache: new InMemoryCache(),
 });
 
