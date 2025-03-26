de4da8b9039e96a729cea61a226ae4d4ffac64ad
Ash <ashleighb.jjd@gmail.com>
2025-03-26 14:32:31 -0400

    Seeding database

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,12 +3,14 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGODB_URI = process.env.NODE_ENV === 'production'
+  ? process.env.MONGODB_URI // Use Atlas in Production
+  : process.env.MONGODB_URI_LOCAL; // Use Local DB in Developmen
 
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
-    await mongoose.connect(MONGODB_URI);
-    console.log('Database connected.');
+    await mongoose.connect(MONGODB_URI || '');
+    console.log('Connected to MongoDB: ${MONGODB_URI}');
     return mongoose.connection;
   } catch (error) {
     console.error('Database connection error:', error);
