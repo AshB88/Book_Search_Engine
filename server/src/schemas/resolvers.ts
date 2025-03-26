f087a092f357285bb6b0cc18a0e9e1c59aa25949
Ash <ashleighb.jjd@gmail.com>
2025-03-26 16:07:03 -0400

    Fixing user login issue

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -46,19 +46,29 @@ const resolvers = {
 
   Mutation: {
     login: async (_parent: any, { email, password }: LoginUserArgs): Promise<{ token: string; user: UserDocument }> => {
+      console.log('Attempting login for email:${email}');
+
       const user = await User.findOne({ email});
+      console.log('User found: ', user);
 
       if (!user) {
+        console.log('Login failed: User not found');
         throw new AuthenticationError('Incorrect credentials');
       }
 
+      console.log('Stored hased password: ', user.password);
+      console.log('Endtered password: ', password);
+
       const correctPw = await user.isCorrectPassword(password);
+      console.log('Password match result: ', correctPw);
 
       if (!correctPw) {
+        console.log('Login failed: Password incorrect');
         throw new AuthenticationError('Incorrect credentials');
       }
 
       const token = signToken(user.username, user.email, user._id);
+      console.log('Login successful, token generated:', token);
       return { token, user };
     },
 
